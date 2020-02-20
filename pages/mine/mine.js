4c0de85c339f76998549926b1c36b3aaf26a3650
shilei <240638896@qq.com>
2020-02-20 17:24:34 +0800

    123

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,7 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userNav: [
+      { url: '/images/download.png', name: '本地音乐'},
+      { url: '/images/download.png', name: '本地音乐' },
+      { url: '/images/download.png', name: '本地音乐' },
+      { url: '/images/download.png', name: '本地音乐' },
+      { url: '/images/download.png', name: '本地音乐' }
+      ]
   },
 
   /**
